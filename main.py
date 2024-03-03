8fc2716a8a1ebe9275b5770f22340ea9b75e82c0
Annatina Christ <annatina.christ@espas.ch>
2024-03-03 07:53:45 +0000

    nested else if conditions

--- main.py
+++ main.py
@@ -3,8 +3,11 @@ height = int(input("What is your height in cm? "))
 
 if height >= 120:
     print("You can ride the rollercoaster")
+    age = int(input("What is your age?"))
+    if age <= 18:
+        print("Please pay $7")
+    else:
+        print("Please pay $12")
 
 else:
     print("You are too short to ride the rollecoaster")
-
-
